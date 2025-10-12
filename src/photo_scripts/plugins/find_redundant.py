0fa80faf2a89ae243c7d387b296799fa1d57a908
Michal Hozza <mhozza@gmail.com>
2025-10-12 21:55:48 +0100

    Update the suffix to look for + some small changes.

--- src/photo_scripts/plugins/find_redundant.py
+++ src/photo_scripts/plugins/find_redundant.py
@@ -1,24 +1,32 @@
 from pathlib import Path
 import os
+import sys
 
 # Add more suffixes to this list to find more redundant files.
-REDUNDANT_SUFFIXES = ["-Enhanced.dng"]
+REDUNDANT_SUFFIXES = ["-Enhanced-NR.dng"]
 
 def register_subcommand(subparsers):
-    parser = subparsers.add_parser("find-redundant", help="Find redundant DNG files.")
-    parser.add_argument("directory", type=Path, help="Directory to search for redundant DNGs.")
-    parser.set_defaults(func=find_redundant_dng)
+    parser = subparsers.add_parser("find-redundant", help="Find redundant image files.")
+    parser.add_argument("directory", type=Path, help="Directory to search for redundant files.")
+    parser.set_defaults(func=find_redundant)
 
-def find_redundant_dng(args):
+def find_redundant(args):
     directory = args.directory
+    file_count = 0
+    redundant_file_count = 0
+    # os.walk recursively searches the directory tree.
     for root, _, files in os.walk(directory):
         for file in files:
+            file_count += 1
             for suffix in REDUNDANT_SUFFIXES:
                 if file.lower().endswith(suffix.lower()):
-                    dng_path = Path(root) / file
+                    path = Path(root) / file
                     base_name = file[:-len(suffix)]
                     cr2_path = Path(root) / (base_name + ".cr2")
                     cr3_path = Path(root) / (base_name + ".cr3")
                     if cr2_path.exists() or cr3_path.exists():
-                        print(dng_path)
+                        print(path)
+                        redundant_file_count += 1
                         break
+    print(f"Scanned {file_count} files.", file=sys.stderr)
+    print(f"Found {redundant_file_count} redundant files.", file=sys.stderr)
